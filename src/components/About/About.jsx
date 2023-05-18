427742c3e55ee0c57cd4c65470bd94391730b444
Cristelos <cristelos@msn.com>
2023-05-18 14:03:33 +0200

    about section finished

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,7 +8,7 @@ export const About = () => {
             <div className='about_boxs'>
                 <div className='about_box'>
                     <h3 className='about_title'>Sobre mí/<br/>About me</h3>
-                    <p className='skills_text1'>Mis habilidades incluyen tanto el diseño de tu proyecto, como su puesta en marcha.</p>
+                    <p className='skills_text1'>¡Hola!<br/> Soy Patricia Fernández Ruibal, apasionada desarrolladora Full Stack y diseñadora UX/UI. Con formación tanto en bellas artes como en full stack developer, me dedico a crear experiencias digitales cautivadoras y funcionales.</p>
                     <div className='about_icons'>
                             <img src='/assets/HTML5_logo_black.svg.png' alt='Logo Html5' className='about_icon'/>
                             <img src='/assets/css3-logo-png-transparent.png' alt='Logo Css3' className='about_icon'/>
@@ -25,25 +25,25 @@ export const About = () => {
                     <div className='about_details'>
                     <div className='about_mix'>
                         <p className='about_number'>1</p>
-                        <h3>Desarrollo de aplicaciones web y móvil: </h3>
+                        <h3>Trayectoria profesional: </h3>
                     </div>
-                    <p className='about_text2'>Construyo aplicaciones web y móviles cautivadoras y funcionales, utilizando tecnologías modernas para brindar experiencias excepcionales en ambas plataformas.</p>
+                    <p className='about_text2'>Mi trayectoria profesional me ha permitido trabajar en diversos proyectos, donde he tenido la oportunidad de desarrollar habilidades técnicas y blandas. Desde la creación de sitios web personalizados hasta el diseño de interfaces intuitivas, mi objetivo principal es proporcionar soluciones digitales de alta calidad que satisfagan las necesidades de mis clientes y los usuarios finales.</p>
                    
                     </div>
                     <div className='about_details'>
                     <div className='about_mix'>
                     <p className='about_number'>2</p>
-                        <h3>Diseño de experiencia de usuario (UX/UI):</h3>
+                        <h3>Trabajo en equipo:</h3>
                     </div>
-                    <p className='about_text2'>Creo interfaces intuitivas y atractivas que brindan una experiencia de usuario excepcional.</p>
+                    <p className='about_text2'>Soy un/a apasionado/a por el trabajo en equipo y valoro la colaboración y la comunicación abierta. Mi enfoque se basa en comprender a fondo las metas y objetivos de cada proyecto, para así poder traducirlos en soluciones creativas y efectivas. Siempre estoy en búsqueda de nuevos desafíos y oportunidades para aprender y crecer profesionalmente.</p>
                     
                     </div>
                     <div className='about_details'>
                     <div className='about_mix'>
                     <p className='about_number'>3</p>
-                        <h3>Optimización de rendimiento y escalabilidad:</h3>
+                        <h3>Soft Skils:</h3>
                     </div>
-                    <p className='about_text2'>Mejoro el rendimiento y la escalabilidad de aplicaciones web existentes, asegurando que funcionen de manera rápida y eficiente incluso en momentos de alta demanda.</p>
+                    <p className='about_text2'>Además de mis habilidades técnicas, soy una persona organizada, proactiva y orientada a resultados. Me gusta enfrentar nuevos desafíos y encontrar soluciones innovadoras para superarlos. También disfruto mantenerme actualizada con las últimas tendencias y tecnologías del mundo del desarrollo web y el diseño UX/UI.</p>
                     
                     </div>
                 </div>
